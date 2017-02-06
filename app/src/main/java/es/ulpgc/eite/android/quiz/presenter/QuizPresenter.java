7e80683672c285fdd4624e06cd0447bb4d6dfe43
antonioaren <antonioaren@hotmail.com>
2017-02-06 02:15:00 +0000

    conexion presentador/modelo

--- app/src/main/java/es/ulpgc/eite/android/quiz/presenter/QuizPresenter.java
+++ app/src/main/java/es/ulpgc/eite/android/quiz/presenter/QuizPresenter.java
@@ -1,17 +1,24 @@
 package es.ulpgc.eite.android.quiz.presenter;
 
 import android.support.v7.app.AppCompatActivity;
+import es.ulpgc.eite.android.quiz.model.QuizModel;
 
 /**
  * Created by User on 05/02/2017.
  */
 
 public class QuizPresenter extends AppCompatActivity {
+    private QuizModel model;
+
+
     public QuizPresenter(){
+        model = new QuizModel();
 
     }
 
     public void onTrueBtnClicked() {
+        model.onAnswerBtnClicked(true);
+
     }
 
     public void onFalseBtnClicked() {
